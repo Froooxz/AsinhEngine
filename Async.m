9c56a30a3e56a0e6a32634b1c08f86a615ded105
FRXZLP <Frooxz@yandex.ru>
2024-06-27 17:02:19 +0300

    564465456465

--- Async.m
+++ Async.m
@@ -1,82 +1,63 @@
+clear all;
+clc;
+close all;
+
 % Параметры модели
-Ls = 0.2; % Индуктивность статора
-Lm = 0.2; % Взаимная индуктивность
-Rs = 0.01; % Сопротивление статора
-Rr = 0.01; % Сопротивление ротора
-Tr = 0.1; % Время роторной цепи
-KRP = 1.5; % Константа для электромагнитного момента
-
-% Начальные условия
-Isd = 0; % Ток статора по d-оси
-Isq = 0; % Ток статора по q-оси
-PsiSq = 0; % Поток ротора по q-оси
-
+Ls = 0.4723; % индуктивность на статоре
+Lm = 0.984; % индуктивность намагничивания двигателя
+Rs = 0.1545; % сопротивление статора
+Rr = 0.067; % Сопротивление ротора
+KR = 0.1301; % коэффицент ротора
+ZP = 1;
+Lr = 0.5151;
+Tr = Lr/Rr;
 % Входные сигналы (примерные значения, заменить на реальные)
-Usd = 230; % Напряжение на статоре по d-оси
-Usq = 230; % Напряжение на статоре по q-оси
-omega_e = 50; % Электрическая угловая скорость
-omega_R = 48; % Угловая скорость ротора
-
+Usd = -220; % Напряжение на статоре по d-оси
+Usq = -220; % Напряжение на статоре по q-оси
+omega_e = 0; % Электрическая угловая скорость
+omega_R = 0.01; % угловая скорость ротора
+omega_R = 0.01; % угловая скорость ротора
 % Временные параметры
-T = 10; % Время моделирования
-dt = 0.001; % Шаг интегрирования
+T = 100; % Время моделирования
+dt = 0.01; % Шаг интегрирования
 N = T / dt; % Количество шагов
 
-% Массивы для хранения результатов
-time = (0:dt:T-dt)';
-Isd_array = zeros(N, 1);
-Isq_array = zeros(N, 1);
-PsiSq_array = zeros(N, 1);
-M_array = zeros(N, 1);
+Isd(1) = 1; % Ток статора по d-оси
+Isq(1) = 1; % Ток статора по q-оси
+PsiR(1) = 1; % Поток ротора по q-оси
+Mem(1) = 1;
 
-% Численное интегрирование методом Эйлера
-for k = 1:N
-    % Текущее время
-    t = (k-1) * dt;
-    
-    % Расчет производных
-    dIsd = (1/Ls) * (Usd - Rs * Isd + omega_e * Ls * Isq);
-    dIsq = (1/Ls) * (Usq - Rs * Isq - (omega_e - omega_R) * Lm * Isd);
-    PsiR = Lm * Isd; % Поток ротора по d-оси
-    dPsiSq = - (omega_e - omega_R) * PsiR + (1/Tr) * (Isq - (Rr/Lm) * PsiSq);
-    
-    % Обновление переменных методом Эйлера
-    Isd = Isd + dIsd * dt;
-    Isq = Isq + dIsq * dt;
-    PsiSq = PsiSq + dPsiSq * dt;
-    
-    % Вычисление электромагнитного момента
-    M = (3/2) * KRP * PsiSq * Isd;
-    
-    % Сохранение результатов
-    Isd_array(k) = Isd;
-    Isq_array(k) = Isq;
-    PsiSq_array(k) = PsiSq;
-    M_array(k) = M;
+
+for i = 1:N
+    Isd(i+1) =  Isd(i) + dt*(1/(Ls-Lm*KR) * (Usd - Isd(i)*Rs  - KR*PsiR(i)) + omega_e*Isq(i));
+    Isq(i+1) = Isq(i) + dt*(1/(Ls-Lm*KR) * (Usq -  Isq(i)*Rs - KR*omega_R*PsiR(i))- omega_e*Isd(i));
+    PsiR(i+1) = PsiR(i) + dt*((1/Tr) * (Lm*Isd(i) - PsiR(i)));
+    omega_e = ZP*omega_R + (Lm/(PsiR(i)*Tr))*Isq(i);
+    Mem(i+1) = ((3*ZP*KR)/2)*PsiR(i)*Isq(i);
 end
 
 % Построение графиков
 figure;
 subplot(3,1,1);
-plot(time, Isd_array);
+plot(Isd);
 title('Ток статора по d-оси');
 xlabel('Время (с)');
 ylabel('I_{sd} (A)');
 
 subplot(3,1,2);
-plot(time, Isq_array);
+plot(Isq);
 title('Ток статора по q-оси');
 xlabel('Время (с)');
 ylabel('I_{sq} (A)');
 
 subplot(3,1,3);
-plot(time, PsiSq_array);
+plot(PsiR);
 title('Поток ротора по q-оси');
 xlabel('Время (с)');
 ylabel('Ψ_{sq} (Wb)');
 
 figure;
-plot(time, M_array);
+plot(Mem);
 title('Электромагнитный момент');
 xlabel('Время (с)');
 ylabel('M (Nm)');
--- Sinus.m
+++ Sinus.m
@@ -1,6 +1,6 @@
 clc; clear all; close all;
 
-T = 0.05; h = 0.0001; N = T / h; 
+T = 0.05; dt = 0.0001; N = T / dt; 
 
 Um = 220; % Амплитуда напряжения
 f = 50; % Частота (Гц)
@@ -8,20 +8,20 @@ omega = 2 * pi * f; % Угловая частота (рад/с)
 phase1 = -2*pi/3;  phase2 = 0;  phase3 = 2*pi/3; % Начальная фаза (рад)
 
 % Инициализация массивов
-u1 = zeros(1, N+1); u2 = zeros(1, N+1); u3 = zeros(1, N+1);
+U1 = zeros(1, N+1); U2 = zeros(1, N+1); U3 = zeros(1, N+1);
 t = zeros(1, N+1);
 
 % Начальные условия
-u1(1) = Um * sin(phase1); u2(1) = Um * sin(phase2); u3(1) = Um * sin(phase3);
+U1(1) = Um * sin(phase1); U2(1) = Um * sin(phase2); U3(1) = Um * sin(phase3);
 t(1) = 0;
 
 for i = 1:N
-    t(i+1) = t(i) + h;
-    u1(i+1) = Um * sin(omega * t(i+1) + phase1);
-    u2(i+1) = Um * sin(omega * t(i+1) + phase2);
-    u3(i+1) = Um * sin(omega * t(i+1) + phase3);
+    t(i+1) = t(i) + dt;
+    U1(i+1) = Um * sin(omega * t(i+1) + phase1);
+    U2(i+1) = Um * sin(omega * t(i+1) + phase2);
+    U3(i+1) = Um * sin(omega * t(i+1) + phase3);
 end
 
-plot(t, u1, t, u2, t, u3);
+plot(t, U1, t, U2, t, U3);
 xlabel('Время (с)'); ylabel('Напряжение (В)');
 grid on;
